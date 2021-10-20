450b5fccaee823574b9115297d40f62ef520078d
Justin De Sio <65298664+Justin-De-Sio@users.noreply.github.com>
2021-10-20 11:41:32 +0000

    +opinion spreading

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import matplotlib.cbook as cbook
-from matplotlib import cm
 import numpy as np
 import random
+import time
 
 
 # fonction chopisissant le poids au hasard
@@ -14,37 +12,39 @@ def GenerateWeight():
 
 # défini les noeuds, arrëëtes, et poid du graphe
 
-A = np.array([[0, 0.4, 0.6],
-              [0.5, 0, 0.5],
-              [0.3, 0.7, 0]])
-# génère le graphe
+#A = np.array([[0, 0.4, 0.6],
+ #             [0.4, 0, 0.5],
+  #            [0.3, 0.7, 0]])
 
+A = np.array([[0, 0.5, 0.3],
+              [0.4, 0, 0.7],
+              [0.6, 0.5, 0]])
+# génère le graphe
 G = nx.DiGraph(A)
 pos = nx.spring_layout(G)
-
 # divise le graph en 2 opinions
-u = np.ones(3)
-for j in range(3 // 2):
-    u[2 * j] = 0
+color = ([1,0,1])
 
+def run():
 
+    # Tracé de la condition initiale
+    fig = plt.figure(1)
+    nx.draw(G, with_labels=True, pos=pos, node_color=color, cmap='coolwarm')
+    labels = nx.get_edge_attributes(G, 'weight')
+    sc = nx.draw_networkx_nodes(G, pos, node_color=color, cmap='coolwarm')#affiche echelle
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, label_pos=0.2)#affiche pos
+    print(color)
+    # affiche le graphe et la légende
+    plt.axis('equal')
+    fig.colorbar(sc)#affiche echelle
+    plt.title("Opinion initiale")
+    plt.show()
 
-# Tracé de la condition initiale
-fig = plt.figure(1)
-nx.draw(G, with_labels=True, pos=pos, node_color=u, cmap='coolwarm')
+def getMatrix():
+    M = nx.to_numpy_array(G)
+    print("Matrice d\'adjacence: \n", M)
 
-labels = nx.get_edge_attributes(G, 'weight')
-sc = nx.draw_networkx_nodes(G, pos, node_color=u, cmap='coolwarm')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, label_pos=0.3)
-
-
-# matrix
-M = nx.to_numpy_array(G)
-
-print("Matrice d\'adjacence: \n", M)
-
-# affiche le graphe et la légende
-
-fig.colorbar(sc)
-plt.title("Opinion initiale")
-plt.show()
+for i in range(2):
+    run()
+    color = np.dot(color, A)
+    time.sleep(1)
